43bc3779a23cd93b85c9ca2936265822da1311dd
Paing Soe Khant <babyboss7916@gmail.com>
2024-04-11 20:34:31 +0630

    [Complete]AdoDotNet Create

---
+++ PSKDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSKDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample
+    {
+        //SqlConnectionStringBuilder method and _ use for global variable
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DESKTOP-5V2KGHB", //server name 
+            InitialCatalog = "DotNetTrainingBatch4", //database name
+            UserID = "sa",
+            Password = "sasa@1234"
+
+        };
+
+        //read method 
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+            Console.WriteLine("Connection open.");
+
+            string query = "select * from tbl_blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+            Console.WriteLine("Connection close.");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id =>" + dr["BlogId"]);
+                Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+                Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+                Console.WriteLine("------------------------------------");
+            }
+        }
+
+        //create method
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+            connection.Open();
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+
+            //return number of role 
+            int result = cmd.ExecuteNonQuery();
+            Console.WriteLine(result);
+            connection.Close();
+
+            //check create data success or not.
+            string message = result > 0 ? "Create Successful." : "Create Failed.";
+            Console.WriteLine(message);
+        }
+
+
+    }
+}
--- PSKDotNetCore.ConsoleApp/Program.cs
+++ PSKDotNetCore.ConsoleApp/Program.cs
@@ -1,37 +1,13 @@
 ﻿
+using PSKDotNetCore.ConsoleApp;
 using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "DESKTOP-5V2KGHB"; //server name 
-stringBuilder.InitialCatalog = "DotNetTrainingBatch4"; //database name
-stringBuilder.UserID= "sa";
-stringBuilder.Password = "sasa@1234";
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 
+//adoDotNetExample.Read();
 
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-
-
-connection.Open();
-Console.WriteLine("Connection open.");
-
-string query = "select * from tbl_blog";
-SqlCommand cmd =new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
-
-connection.Close();
-Console.WriteLine("Connection close.");
-
-foreach(DataRow dr in dt.Rows )
-{
-    Console.WriteLine("Blog Id =>" +dr["BlogId"]);
-    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
-    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
-    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
-    Console.WriteLine("------------------------------------");
-}
+adoDotNetExample.Create("AdoDotNet","Ko Sann Lynn Htun","C# AdoDotNet Course" );
 Console.ReadKey();
