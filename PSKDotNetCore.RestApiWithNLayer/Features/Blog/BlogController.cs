f8fbaeb96ac4ded0afee922201400ed2a4310a7f
Paing Soe Khant <babyboss7916@gmail.com>
2024-05-08 22:01:56 +0630

    [Complete] N-Layer Architecture & Patch Method

---
+++ PSKDotNetCore.RestApiWithNLayer/Features/Blog/BlogController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PSKDotNetCore.RestApiWithNLayer.Features.Blog
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly BL_Blog _bL_Blog;
+
+        public BlogController()
+        {
+            _bL_Blog = new BL_Blog();
+        }
+
+        [HttpGet]
+        public IActionResult Read()
+        {
+            var lst = _bL_Blog.GetBlogs();
+            return Ok(lst);
+        }
+       
+        [HttpGet("{id}")]
+        public IActionResult Edit(int id)
+        {
+            var item = _bL_Blog.GetBlog(id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+            return Ok(item);
+        }
+       
+        [HttpPost]
+        public IActionResult Create(BlogModel blog)
+        {
+            var result = _bL_Blog.CreateBlog(blog);
+          
+            string message = result > 0 ? "Create Successful." : "Create Failed.";
+            return Ok(message);
+        }
+    
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, BlogModel blog)
+        {
+            var item = _bL_Blog.GetBlog(id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+
+            var result = _bL_Blog.UpdateBlog(id, blog );
+            string message = result > 0 ? "Update Successful." : "Update Failed.";
+            return Ok(message);
+        }
+      
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, BlogModel blog)
+        {
+            var item = _bL_Blog.GetBlog(id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogTitle = blog.BlogAuthor;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogTitle = blog.BlogContent;
+            }
+
+            var result = _bL_Blog.UpdateBlog(id, blog);
+            string message = result > 0 ? "Update Successful." : "Update Failed.";
+            return Ok(message);
+        }
+      
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _bL_Blog.GetBlog(id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+           
+            var result = _bL_Blog.DeleteBlog(id);
+            string message = result > 0 ? "Delete Successful." : "Delete Failed.";
+            return Ok(message);
+        }
+    }
+}
---
+++ PSKDotNetCore.RestApiWithNLayer/Features/Blog/DA_Blog.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using PSKDotNetCore.RestApiWithNLayer.Db;
+
+namespace PSKDotNetCore.RestApiWithNLayer.Features.Blog
+{
+    public class DA_Blog
+    {
+        //Data Access
+        private readonly AppDbContext _context;
+
+        public DA_Blog()
+        {
+            _context = new AppDbContext();
+        }
+
+        public List<BlogModel> GetBlogs()
+        {
+            var lst = _context.Blogs.ToList();
+            return lst;
+        }
+
+        public BlogModel GetBlog(int id)
+        {
+            var result = _context.Blogs.FirstOrDefault(x=> x.BlogId == id);
+            return result;
+        }
+
+        public int CreateBlog(BlogModel requestModel) 
+        { 
+            _context.Blogs.Add(requestModel);
+            var result = _context.SaveChanges();
+            return result;
+        }
+
+        public int UpdateBlog(int id ,BlogModel requestModel)
+        {
+            var item  = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item is null) return 0;
+
+            item.BlogTitle = requestModel.BlogTitle;
+            item.BlogAuthor = requestModel.BlogAuthor;
+            item.BlogContent = requestModel.BlogContent;
+
+            var result = _context.SaveChanges();
+            return result;
+        }
+
+        public int DeleteBlog(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item is null) return 0;
+
+            _context.Blogs.Remove(item);
+            var result = _context.SaveChanges();
+            return result;
+        }
+    }
+}
