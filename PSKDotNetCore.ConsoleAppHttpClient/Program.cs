34cef5001eb66e4afb682701c313c4332e56e75b
Paing Soe Khant <babyboss7916@gmail.com>
2024-05-11 23:36:43 +0630

    [Complete] Burma Project Idea Json to Api

---
+++ PSKDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string jsonStr = await File.ReadAllTextAsync("Birds.json");
+var model = JsonConvert.DeserializeObject<Main>(jsonStr);
+
+Console.WriteLine(jsonStr);
+
+string jsonStr2 = await File.ReadAllTextAsync("MyanmarProverbs.json");
+var model2 = JsonConvert.DeserializeObject<Main>(jsonStr);
+
+
+Console.ReadLine();
+
+
+public class Main
+{
+    public Tbl_Bird[] Tbl_Bird { get; set; }
+}
+
+public class Tbl_Bird
+{
+    public int Id { get; set; }
+    public string BirdMyanmarName { get; set; }
+    public string BirdEnglishName { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}
+
+
+
---
+++ PSKDotNetCore.RestApiWithNLayer/Features/Birds/BirdsController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace PSKDotNetCore.RestApiWithNLayer.Features.Birds
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirdsController : ControllerBase
+    {
+        private readonly Birds _data;
+
+        private async Task<Birds> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("Birds.json");
+            var model = JsonConvert.DeserializeObject<Birds>(jsonStr);
+            return model;
+
+        }
+
+        [HttpGet("birds")]
+        public async Task<IActionResult> Birds()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_Bird);
+        }
+
+    }
+
+    public class Birds
+    {
+        public Tbl_Bird[] Tbl_Bird { get; set; }
+    }
+
+    public class Tbl_Bird
+    {
+        public int Id { get; set; }
+        public string BirdMyanmarName { get; set; }
+        public string BirdEnglishName { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+}
