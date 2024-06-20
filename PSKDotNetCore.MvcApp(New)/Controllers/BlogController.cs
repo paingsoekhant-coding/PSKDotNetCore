1db4bca12459f675a25accdf506ed6d34eec2dec
Paing Soe Khant <babyboss7916@gmail.com>
2024-06-20 08:10:23 +0630

    [Complete]ASP.NET Core MVC Blog Create

--- PSKDotNetCore.MvcApp(New)/Controllers/BlogController.cs
+++ PSKDotNetCore.MvcApp(New)/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PSKDotNetCore.MvcApp_New_.Db;
+using PSKDotNetCore.MvcApp_New_.Models;
 
 
 namespace PSKDotNetCore.MvcApp_New_.Controllers;
@@ -17,5 +18,21 @@ public class BlogController : Controller
     {
         var lst = await _db.Blogs.ToListAsync();
         return View(lst);
+    }
+
+    [ActionName("Create")]
+    public IActionResult BlogCreate()
+    {
+
+        return View("BlogCreate");
+    }
+
+    [HttpPost]
+    [ActionName("Save")]
+    public async Task<IActionResult>  BlogCreate(BlogModel blog) 
+    { 
+        await _db.Blogs.AddAsync(blog);
+        var result = await _db.SaveChangesAsync();
+        return Redirect("/Blog");
     }
 }
