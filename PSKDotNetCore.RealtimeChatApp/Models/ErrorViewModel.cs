0cdb6462730ce0742beb85c514614fb3a2dfeee2
Paing Soe Khant <babyboss7916@gmail.com>
2024-07-12 11:57:26 +0630

    [Complete]Realtime Chart App

---
+++ PSKDotNetCore.RealtimeChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSKDotNetCore.RealtimeChatApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
